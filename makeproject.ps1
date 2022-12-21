bf0e0a464762803b2f360725ec2778b2674491a0
Robin Aerts <nibor.aerts@gmail.com>
2022-12-21 21:16:20 +0100

    created basic template

---
+++ makeproject.ps1
@@ -0,0 +1,38 @@
+cls
+
+write-host ""
+write-host "  RRRRRRRRRRRRRRRRR        OOOOOOOOO     BBBBBBBBBBBBBBBBB   YYYYYYY       YYYYYYYTTTTTTTTTTTTTTTTTTTTTTTEEEEEEEEEEEEEEEEEEEEEE "
+write-host "  R::::::::::::::::R     OO:::::::::OO   B::::::::::::::::B  Y:::::Y       Y:::::YT:::::::::::::::::::::TE::::::::::::::::::::E "
+write-host "  R::::::RRRRRR:::::R  OO:::::::::::::OO B::::::BBBBBB:::::B Y:::::Y       Y:::::YT:::::::::::::::::::::TE::::::::::::::::::::E "
+write-host "  RR:::::R     R:::::RO:::::::OOO:::::::OBB:::::B     B:::::BY::::::Y     Y::::::YT:::::TT:::::::TT:::::TEE::::::EEEEEEEEE::::E "
+write-host "    R::::R     R:::::RO::::::O   O::::::O  B::::B     B:::::BYYY:::::Y   Y:::::YYYTTTTTT  T:::::T  TTTTTT  E:::::E       EEEEEE "
+write-host "    R::::R     R:::::RO:::::O     O:::::O  B::::B     B:::::B   Y:::::Y Y:::::Y           T:::::T          E:::::E              "
+write-host "    R::::RRRRRR:::::R O:::::O     O:::::O  B::::BBBBBB:::::B     Y:::::Y:::::Y            T:::::T          E::::::EEEEEEEEEE    "
+write-host "    R:::::::::::::RR  O:::::O     O:::::O  B:::::::::::::BB       Y:::::::::Y             T:::::T          E:::::::::::::::E    "
+write-host "    R::::RRRRRR:::::R O:::::O     O:::::O  B::::BBBBBB:::::B       Y:::::::Y              T:::::T          E:::::::::::::::E    "
+write-host "    R::::R     R:::::RO:::::O     O:::::O  B::::B     B:::::B       Y:::::Y               T:::::T          E::::::EEEEEEEEEE    "
+write-host "    R::::R     R:::::RO:::::O     O:::::O  B::::B     B:::::B       Y:::::Y               T:::::T          E:::::E              "
+write-host "    R::::R     R:::::RO::::::O   O::::::O  B::::B     B:::::B       Y:::::Y               T:::::T          E:::::E       EEEEEE "
+write-host "  RR:::::R     R:::::RO:::::::OOO:::::::OBB:::::BBBBBB::::::B       Y:::::Y             TT:::::::TT      EE::::::EEEEEEEE:::::E "
+write-host "  R::::::R     R:::::R OO:::::::::::::OO B:::::::::::::::::B     YYYY:::::YYYY          T:::::::::T      E::::::::::::::::::::E "
+write-host "  R::::::R     R:::::R   OO:::::::::OO   B::::::::::::::::B      Y:::::::::::Y          T:::::::::T      E::::::::::::::::::::E "
+write-host "  RRRRRRRR     RRRRRRR     OOOOOOOOO     BBBBBBBBBBBBBBBBB       YYYYYYYYYYYYY          TTTTTTTTTTT      EEEEEEEEEEEEEEEEEEEEEE "
+
+
+write-host ""
+write-host ""
+write-host "----------------------------------------------------- MAKE NEW PROJECT SCRIPT ------------------------------------------------- "
+write-host ""
+write-host "	Creator: 		Robin Aerts "
+write-host ""
+write-host "	More Info:		https://robyte.me/scripts/makeproject "
+write-host ""
+write-host "	Version: 		1.0.2 "
+write-host ""
+write-host "	Last updated: 		21st December 2022 "
+write-host ""
+write-host "------------------------------------------------------------------------------------------------------------------------------- "
+write-host ""
+write-host ""
+
+PAUSE
